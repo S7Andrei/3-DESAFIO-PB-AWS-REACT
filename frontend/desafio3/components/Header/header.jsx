a4e9c56a8dc5bc952a0ce210ff84fa3b10498b6b
Andrei Silva <andreiluizsilva07@gmail.com>
2024-01-22 23:21:03 -0300

    Ajustes e Criação
    Foi configurado as fontes de tamanhos e sua font-family no index.css, assim como as cores foram também configuradas em suas devidas paletas. Organizei as pastas de Header e Footer dentro de uma pasta chamada "Components". Algumas imagens foram inseridads ao assets. O Header está criado.

---
+++ frontend/desafio3/components/Header/header.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from '../../components/Header/header.module.css';  // Importe as classes do arquivo CSS como um objeto
+
+import logo from '../../src/assets/header/Logo.svg';
+import user from '../../src/assets/header/user-avatar.svg';
+import bell from '../../src/assets/header/bell-icon.svg';
+import separetor from '../../src/assets/header/seperator.svg';
+
+const Header = () => {
+  return (
+    <>
+
+      <nav className={styles.NavBar}>  
+
+        <div className={styles.arro}>
+
+        <div className={styles.logo}>  
+        <a href="#">
+          <img src={logo} alt="logo" />
+        </a>
+        </div>
+
+        <div className={styles['nav-links']}> 
+          <a className={styles.target}>Home</a>
+          <a>Getting a Taxi</a>
+          <a>Mobile App</a>
+          <a>Contact Us</a>
+        </div>
+
+        <div className={styles['user-section']}>  
+          <img src={bell} alt="bell" />
+          <img src={separetor} alt="seperator" />
+          <img src={user} alt="avatar" />
+        </div>
+
+        </div>
+      </nav>
+
+    </>
+  );
+};
+
+export default Header;
--- frontend/desafio3/src/App.jsx
+++ frontend/desafio3/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+import Header from '../components/Header/header'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className={"amora"}>test</div>
+      <Header />
     </>
   )
 }
